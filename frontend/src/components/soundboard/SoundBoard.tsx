4fb7088c6e37457359631544080b53236df4e647
Igor Binkowski <ibinkowski@apptension.com>
2023-02-01 21:59:56 +0100

    made the buttons not repeat

--- frontend/src/components/soundboard/SoundBoard.tsx
+++ frontend/src/components/soundboard/SoundBoard.tsx
@@ -16,8 +16,6 @@ const SoundBoard: React.FC<Props> = ({ samples }) => {
     <div className="mt-12 flex justify-center">
       <div className="l:w-3/5 grid w-2/3 -skew-y-3 grid-cols-2 justify-items-center gap-x-12 gap-y-16 align-middle md:grid-cols-3 xl:w-1/2 2xl:grid-cols-4">
         {buttons}
-        {buttons}
-        {buttons}
       </div>
     </div>
   )
