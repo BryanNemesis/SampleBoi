2009ad7c9f7597cd9636da665a85170794718e12
Igor Bińkowski <igorbinkowski1@gmail.com>
2023-03-22 23:01:59 +0100

    support of sample modes on SoundButton

--- frontend/src/components/soundboard/SoundButton.tsx
+++ frontend/src/components/soundboard/SoundButton.tsx
@@ -1,6 +1,6 @@
-import useSound from 'use-sound'
-import { useState } from 'react'
-import Sample from '../../types/Sample'
+import useSound from "use-sound"
+import { useState } from "react"
+import Sample from "../../types/Sample"
 
 interface Props {
   sample: Sample
@@ -14,7 +14,15 @@ const SoundButton: React.FC<Props> = ({ sample }) => {
     },
   })
 
-  const handleClick: React.MouseEventHandler<HTMLDivElement> = () => {
+  const oneshotPlay: React.MouseEventHandler<HTMLDivElement> = () => {
+    if (isPlaying) {
+      stop()
+    }
+    setIsPlaying(true)
+    play()
+  }
+
+  const startStopPlay: React.MouseEventHandler<HTMLDivElement> = () => {
     if (isPlaying) {
       setIsPlaying(false)
       stop()
@@ -25,17 +33,34 @@ const SoundButton: React.FC<Props> = ({ sample }) => {
   }
 
   const cropText = (text: string) => {
-    return text.length > 12 ? text.substring(0, 12) + '···' : text
+    return text.length > 12 ? text.substring(0, 12) + "···" : text
+  }
+
+  const headerIcons = () => {
+    return sample.mode === "oneshot" ? (
+      <span className="material-icons text-sm text-zinc-100">start</span>
+    ) : (
+      <>
+        <span className="material-icons text-sm text-zinc-100 -mr-1">play_arrow</span>
+        <span className="material-icons text-sm text-zinc-100 -ml-px">pause</span>
+      </>
+    )
   }
 
   return (
     <div>
-      <div className="m-px text-xs uppercase text-zinc-100">{cropText(sample.name)}</div>
+      <div className="flex">
+        <div className="m-px text-xs uppercase text-zinc-100 flex-1">
+          {cropText(sample.name)}
+        </div>
+        {headerIcons()}
+      </div>
       <div
-        onClick={handleClick}
+        onClick={sample.mode === "oneshot" ? oneshotPlay : startStopPlay}
         className={`h-32 w-32 rounded-md border-2 border-zinc-300 active:border-zinc-400 active:shadow-inner active:shadow-zinc-500 ${
-          isPlaying ? 'bg-red-800' : 'bg-zinc-800'
+          isPlaying && "bg-red-800"
         }`}
+        style={{ backgroundColor: sample.color }}
       ></div>
     </div>
   )
